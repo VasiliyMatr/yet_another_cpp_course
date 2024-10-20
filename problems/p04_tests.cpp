0a0038b472b6d35d4dc56cb3549ed0b3ded2f1ef
VasiliyMatr <vasiliymatrenin@gmail.com>
2024-10-20 21:41:05 +0300

    [p04] Add solution for p04_08

--- problems/p04_tests.cpp
+++ problems/p04_tests.cpp
@@ -8,3 +8,18 @@ TEST(p04_07, basic_fibonacci) {
     ASSERT_EQ(ya_cpp::Fibonacci<10>{}.value, 34);
     ASSERT_EQ(ya_cpp::Fibonacci<20>{}.value, 4181);
 }
+
+#include "p04_08.hpp"
+
+TEST(p04_08, basic_binomial) {
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<1, 1>{}), 1);
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<2, 2>{}), 1);
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<3, 3>{}), 1);
+
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<2, 1>{}), 2);
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<3, 1>{}), 3);
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<3, 2>{}), 3);
+
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<20, 7>{}), 77'520);
+    ASSERT_EQ((ya_cpp::BinomialCoefficient<20, 10>{}), 184'756);
+}
